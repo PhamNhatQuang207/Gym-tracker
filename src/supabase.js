a533f03d83aa997e3c4f0354ca2bf0f60b207660
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-05-07 09:35:55 +0700

    Add src/supabase.js to gitignore

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hkzvnexparwppxflrwko.supabase.co'; // replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrenZuZXhwYXJ3cHB4Zmxyd2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1NDM5MzUsImV4cCI6MjA2MjExOTkzNX0.XNq08SvYnRJZQoxwptwy1Vz0yhVFXZy4CnUKCiZiChg'; // replace this
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
